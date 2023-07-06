b099a0199cd9f226aa9031363c2d8f3128a9aa29
User <User@K108>
2023-07-06 10:19:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("students.txt");
+            int br2 = 0; int br3 = 0; int br4 = 0; int br5 = 0; int br6= 0;
+            string line = "";
+            while (line != null)
+            {
+                line = reader.ReadLine();
+                if (line == "2")
+                {
+                    br2++;
+                }
+                else if (line == "3")
+                {
+                    br3++;
+                }
+                else if (line == "4")
+                {
+                    br4++;
+                }
+                else if (line == "5")
+                {
+                    br5++;
+                }
+                else if (line == "6")
+                {
+                    br6++;
+                }
+            }
+            reader.Close();
+
+            StreamWriter Writer = new StreamWriter("output.txt");
+            Writer.WriteLine("Двойки: "+br2);
+            Writer.WriteLine("Тройки: " + br3);
+            Writer.WriteLine("Четворки: " + br4);
+            Writer.WriteLine("Петици: " + br5);
+            Writer.WriteLine("Шестици: " + br6);
+            Writer.Close();
+           
+        }
+    }
+}
